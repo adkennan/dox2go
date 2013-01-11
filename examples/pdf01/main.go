b5b7e53f5b62a5949ffe3f195fc49eb9eabe22d9
Andrew Kennan <adkennan@gmail.com>
2013-01-11 22:17:07 +0800

    Updated example

--- examples/pdf01/main.go
+++ examples/pdf01/main.go
@@ -1,3 +1,10 @@
+/*
+* dox2go - A document generating library for go.
+*
+* Copyright 2013 Andrew Kennan. All rights reserved.
+*
+* Simple hello world example.
+ */
 package main
 
 import (
@@ -5,7 +12,7 @@ import (
 	"dox2go"
 	"dox2go/pdf"
 	"fmt"
-	//"math"
+	"math"
 	"os"
 )
 
@@ -15,36 +22,36 @@ func main() {
 
 	doc := pdf.NewPdfDoc(&b)
 
-	page := doc.CreatePage(dox2go.U_MM, // Work in millimeters
-		dox2go.StandardSize(dox2go.PS_A4, dox2go.U_MM), // A4 Page
-		dox2go.PO_Portrait)                             // Portrait orientation
+	page := doc.CreatePage(dox2go.U_MM,
+		dox2go.StandardSize(dox2go.PS_A4, dox2go.U_MM),
+		dox2go.PO_Portrait)
 
 	s := page.Surface()
-	/*
-		s.LineWidth(2.0)
 
-		p := dox2go.NewPath()
-		p.Arc(dox2go.Point{110, 150}, 25, 0, math.Pi*2)
-		p.Close()
-		s.Fg(dox2go.RGB(0, 0, 0))
-		s.Stroke(p)
-		s.Bg(dox2go.RGB(255, 255, 0))
-		s.Fill(p)
+	s.LineWidth(2.0)
 
-		p = dox2go.NewPath()
-		p.Move(dox2go.Point{99, 158})
-		p.Arc(dox2go.Point{97, 158}, 2, 0, math.Pi*2)
-		p.Move(dox2go.Point{125, 158})
-		p.Arc(dox2go.Point{123, 158}, 2, 0, math.Pi*2)
+	p := dox2go.NewPath()
+	p.Arc(dox2go.Point{110, 150}, 25, 0, math.Pi*2)
+	p.Close()
+	s.Fg(dox2go.RGB(0, 0, 0))
+	s.Stroke(p)
+	s.Bg(dox2go.RGB(255, 255, 0))
+	s.Fill(p)
 
-		p.Move(dox2go.Point{97, 140})
-		p.Curve(dox2go.Point{105, 130}, dox2go.Point{115, 130}, dox2go.Point{123, 140})
-		p.Close()
+	p = dox2go.NewPath()
+	p.Move(dox2go.Point{99, 158})
+	p.Arc(dox2go.Point{97, 158}, 2, 0, math.Pi*2)
+	p.Move(dox2go.Point{125, 158})
+	p.Arc(dox2go.Point{123, 158}, 2, 0, math.Pi*2)
 
-		s.Bg(dox2go.RGB(255, 255, 255))
-		s.Fill(p)
-		s.Stroke(p)
-	*/
+	p.Move(dox2go.Point{97, 140})
+	p.Curve(dox2go.Point{105, 130}, dox2go.Point{115, 130}, dox2go.Point{123, 140})
+	p.Close()
+
+	s.Bg(dox2go.RGB(255, 255, 255))
+	s.Fill(p)
+	s.Stroke(p)
+
 	font := doc.CreateFont(pdf.FONT_Helvetica, dox2go.FS_Bold, 20)
 	s.Bg(dox2go.RGB(0, 0, 0))
 	s.Text(font, dox2go.Point{50, 100}, "Hello World!")
